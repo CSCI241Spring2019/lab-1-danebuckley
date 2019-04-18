dcddb5ed57f481d22f4fd6903f1cd67dcbe8c7d4
Dane Buckley <buckled4@wwu.edu>
2019-04-18 14:11:47 -0700

    Hello now prints command line arg

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -2,6 +2,7 @@ package lab1;
 
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+
+        System.out.println("Hello"+args[0]);
     }
 }
